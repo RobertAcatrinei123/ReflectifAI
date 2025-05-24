28b8e9cf49521bf7d041a2bdfa11b08825b9eeeb
palmsy <felix.crisan1@gmail.com>
2025-05-24 19:34:23 +0300

    Main Page Doesnt Work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:reflectifai/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +7,230 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Demo',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _pulse;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..repeat(reverse: true);
+    _pulse = Tween<double>(
+      begin: 1.0,
+      end: 1.25,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF3ec0ec), // light blue
+                  Color(0xFF7f29d2), // purple
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+                // Title with a more attractive background
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color(0xFF3ec0ec), // light blue
+                        Color(0xFF7f29d2), // purple
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(32),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.18),
+                        blurRadius: 18,
+                        offset: const Offset(0, 8),
+                      ),
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.08),
+                        blurRadius: 2,
+                        offset: const Offset(-2, -2),
+                      ),
+                    ],
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.25),
+                      width: 2,
+                    ),
+                  ),
+                  child: const Text(
+                    'ReflectifAI',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: 2,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black54,
+                          offset: Offset(2.0, 2.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment(
+                      0,
+                      -0.4,
+                    ), // move the stack a bit higher
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        // Animated pulsing ring - improved look and new color
+                        Transform.scale(
+                          scale: _pulse.value,
+                          child: Container(
+                            width: 210,
+                            height: 210,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: RadialGradient(
+                                colors: [
+                                  Color(
+                                    0xFFA4D2F4,
+                                  ).withOpacity(0.7 * (2 - _pulse.value)),
+                                  Color(
+                                    0xFFA4D2F4,
+                                  ).withOpacity(0.2 * (2 - _pulse.value)),
+                                  Colors.transparent,
+                                ],
+                                stops: [0.6, 0.85, 1.0],
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(
+                                    0xFFA4D2F4,
+                                  ).withOpacity(0.5 * (2 - _pulse.value)),
+                                  blurRadius: 60,
+                                  spreadRadius: 20,
+                                ),
+                                BoxShadow(
+                                  color: Color(
+                                    0xFF3ec0ec,
+                                  ).withOpacity(0.15 * (2 - _pulse.value)),
+                                  blurRadius: 100,
+                                  spreadRadius: 40,
+                                ),
+                              ],
+                              border: Border.all(
+                                color: Color(
+                                  0xFFA4D2F4,
+                                ).withOpacity(0.9 * (2 - _pulse.value)),
+                                width: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                        // Logo
+                        Container(
+                          width: 150,
+                          height: 150,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.transparent,
+                          ),
+                          child: ClipOval(
+                            child: Image.asset(
+                              'assets/logo.jpeg',
+                              width: 150,
+                              height: 150,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
