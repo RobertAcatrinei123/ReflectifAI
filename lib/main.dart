79c6db4cd109544dbb7c9a0443314931b8d6e0d1
palmsy <felix.crisan1@gmail.com>
2025-05-24 21:15:32 +0300

    Moving to main

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
+  
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late AnimationController _pulseController;
   late AnimationController _speakTicker;
